cd358422169fab077b30e669d70ceda9fd9edf82
QTech <QTech@DESKTOP-803QQI1>
2024-07-01 13:10:24 +0300

    Add project files.

---
+++ CafeSiparis/SiparişForm.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace CafeSiparis
+{
+    partial class SiparişForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SiparişForm));
+            this.siparisGroupBox = new System.Windows.Forms.GroupBox();
+            this.kolaEksiLabel = new System.Windows.Forms.Button();
+            this.kolaArtiLabel = new System.Windows.Forms.Button();
+            this.kolaSayisiLabel = new System.Windows.Forms.Label();
+            this.kolaLabel = new System.Windows.Forms.Label();
+            this.kahveEksiLabel = new System.Windows.Forms.Button();
+            this.kahveArtiLabel = new System.Windows.Forms.Button();
+            this.kahveSayisiLabel = new System.Windows.Forms.Label();
+            this.kahveLabel = new System.Windows.Forms.Label();
+            this.cayEksiLabel = new System.Windows.Forms.Button();
+            this.cayArtiLabel = new System.Windows.Forms.Button();
+            this.caySayisiLabel = new System.Windows.Forms.Label();
+            this.cayLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.siparisGroupBox.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // siparisGroupBox
+            // 
+            this.siparisGroupBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.siparisGroupBox.AutoSize = true;
+            this.siparisGroupBox.Controls.Add(this.pictureBox1);
+            this.siparisGroupBox.Controls.Add(this.kolaEksiLabel);
+            this.siparisGroupBox.Controls.Add(this.kolaArtiLabel);
+            this.siparisGroupBox.Controls.Add(this.kolaSayisiLabel);
+            this.siparisGroupBox.Controls.Add(this.kolaLabel);
+            this.siparisGroupBox.Controls.Add(this.kahveEksiLabel);
+            this.siparisGroupBox.Controls.Add(this.kahveArtiLabel);
+            this.siparisGroupBox.Controls.Add(this.kahveSayisiLabel);
+            this.siparisGroupBox.Controls.Add(this.kahveLabel);
+            this.siparisGroupBox.Controls.Add(this.cayEksiLabel);
+            this.siparisGroupBox.Controls.Add(this.cayArtiLabel);
+            this.siparisGroupBox.Controls.Add(this.caySayisiLabel);
+            this.siparisGroupBox.Controls.Add(this.cayLabel);
+            this.siparisGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.siparisGroupBox.Name = "siparisGroupBox";
+            this.siparisGroupBox.Size = new System.Drawing.Size(425, 233);
+            this.siparisGroupBox.TabIndex = 0;
+            this.siparisGroupBox.TabStop = false;
+            this.siparisGroupBox.Text = "SİPARİŞ";
+            // 
+            // kolaEksiLabel
+            // 
+            this.kolaEksiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kolaEksiLabel.Location = new System.Drawing.Point(213, 170);
+            this.kolaEksiLabel.Name = "kolaEksiLabel";
+            this.kolaEksiLabel.Size = new System.Drawing.Size(48, 36);
+            this.kolaEksiLabel.TabIndex = 12;
+            this.kolaEksiLabel.Text = "-";
+            this.kolaEksiLabel.UseVisualStyleBackColor = true;
+            this.kolaEksiLabel.Click += new System.EventHandler(this.kolaEksiLabel_Click);
+            // 
+            // kolaArtiLabel
+            // 
+            this.kolaArtiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kolaArtiLabel.Location = new System.Drawing.Point(159, 170);
+            this.kolaArtiLabel.Name = "kolaArtiLabel";
+            this.kolaArtiLabel.Size = new System.Drawing.Size(48, 36);
+            this.kolaArtiLabel.TabIndex = 11;
+            this.kolaArtiLabel.Text = "+";
+            this.kolaArtiLabel.UseVisualStyleBackColor = true;
+            this.kolaArtiLabel.Click += new System.EventHandler(this.kolaArtiLabel_Click);
+            // 
+            // kolaSayisiLabel
+            // 
+            this.kolaSayisiLabel.AutoSize = true;
+            this.kolaSayisiLabel.Location = new System.Drawing.Point(92, 182);
+            this.kolaSayisiLabel.Name = "kolaSayisiLabel";
+            this.kolaSayisiLabel.Size = new System.Drawing.Size(14, 16);
+            this.kolaSayisiLabel.TabIndex = 10;
+            this.kolaSayisiLabel.Text = "0";
+            // 
+            // kolaLabel
+            // 
+            this.kolaLabel.AutoSize = true;
+            this.kolaLabel.Location = new System.Drawing.Point(9, 182);
+            this.kolaLabel.Name = "kolaLabel";
+            this.kolaLabel.Size = new System.Drawing.Size(41, 16);
+            this.kolaLabel.TabIndex = 9;
+            this.kolaLabel.Text = "KOLA";
+            this.kolaLabel.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // kahveEksiLabel
+            // 
+            this.kahveEksiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kahveEksiLabel.Location = new System.Drawing.Point(213, 104);
+            this.kahveEksiLabel.Name = "kahveEksiLabel";
+            this.kahveEksiLabel.Size = new System.Drawing.Size(48, 36);
+            this.kahveEksiLabel.TabIndex = 8;
+            this.kahveEksiLabel.Text = "-";
+            this.kahveEksiLabel.UseVisualStyleBackColor = true;
+            this.kahveEksiLabel.Click += new System.EventHandler(this.kahveEksiLabel_Click);
+            // 
+            // kahveArtiLabel
+            // 
+            this.kahveArtiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.kahveArtiLabel.Location = new System.Drawing.Point(159, 104);
+            this.kahveArtiLabel.Name = "kahveArtiLabel";
+            this.kahveArtiLabel.Size = new System.Drawing.Size(48, 36);
+            this.kahveArtiLabel.TabIndex = 7;
+            this.kahveArtiLabel.Text = "+";
+            this.kahveArtiLabel.UseVisualStyleBackColor = true;
+            this.kahveArtiLabel.Click += new System.EventHandler(this.kahveArtiLabel_Click);
+            // 
+            // kahveSayisiLabel
+            // 
+            this.kahveSayisiLabel.AutoSize = true;
+            this.kahveSayisiLabel.Location = new System.Drawing.Point(92, 116);
+            this.kahveSayisiLabel.Name = "kahveSayisiLabel";
+            this.kahveSayisiLabel.Size = new System.Drawing.Size(14, 16);
+            this.kahveSayisiLabel.TabIndex = 6;
+            this.kahveSayisiLabel.Text = "0";
+            // 
+            // kahveLabel
+            // 
+            this.kahveLabel.AutoSize = true;
+            this.kahveLabel.Location = new System.Drawing.Point(9, 116);
+            this.kahveLabel.Name = "kahveLabel";
+            this.kahveLabel.Size = new System.Drawing.Size(52, 16);
+            this.kahveLabel.TabIndex = 5;
+            this.kahveLabel.Text = "KAHVE";
+            this.kahveLabel.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // cayEksiLabel
+            // 
+            this.cayEksiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cayEksiLabel.Location = new System.Drawing.Point(213, 36);
+            this.cayEksiLabel.Name = "cayEksiLabel";
+            this.cayEksiLabel.Size = new System.Drawing.Size(48, 36);
+            this.cayEksiLabel.TabIndex = 4;
+            this.cayEksiLabel.Text = "-";
+            this.cayEksiLabel.UseVisualStyleBackColor = true;
+            this.cayEksiLabel.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // cayArtiLabel
+            // 
+            this.cayArtiLabel.Font = new System.Drawing.Font("Arial Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.cayArtiLabel.Location = new System.Drawing.Point(159, 36);
+            this.cayArtiLabel.Name = "cayArtiLabel";
+            this.cayArtiLabel.Size = new System.Drawing.Size(48, 36);
+            this.cayArtiLabel.TabIndex = 2;
+            this.cayArtiLabel.Text = "+";
+            this.cayArtiLabel.UseVisualStyleBackColor = true;
+            this.cayArtiLabel.Click += new System.EventHandler(this.cayArtiLabel_Click);
+            // 
+            // caySayisiLabel
+            // 
+            this.caySayisiLabel.AutoSize = true;
+            this.caySayisiLabel.Location = new System.Drawing.Point(92, 48);
+            this.caySayisiLabel.Name = "caySayisiLabel";
+            this.caySayisiLabel.Size = new System.Drawing.Size(14, 16);
+            this.caySayisiLabel.TabIndex = 1;
+            this.caySayisiLabel.Text = "0";
+            this.caySayisiLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // cayLabel
+            // 
+            this.cayLabel.AutoSize = true;
+            this.cayLabel.Location = new System.Drawing.Point(9, 48);
+            this.cayLabel.Name = "cayLabel";
+            this.cayLabel.Size = new System.Drawing.Size(34, 16);
+            this.cayLabel.TabIndex = 0;
+            this.cayLabel.Text = "ÇAY";
+            this.cayLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(71, 251);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 38);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SIFIRLA";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(195, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(203, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SİPARİŞİ TAMAMLA";
+            this.button2.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(267, 61);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 125);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SiparişForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 308);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.siparisGroupBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(800, 500);
+            this.MinimizeBox = false;
+            this.Name = "SiparişForm";
+            this.Text = "KAFE SİPARİŞ EKRANI";
+            this.siparisGroupBox.ResumeLayout(false);
+            this.siparisGroupBox.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox siparisGroupBox;
+        private System.Windows.Forms.Button cayArtiLabel;
+        private System.Windows.Forms.Label caySayisiLabel;
+        private System.Windows.Forms.Label cayLabel;
+        private System.Windows.Forms.Button cayEksiLabel;
+        private System.Windows.Forms.Button kolaEksiLabel;
+        private System.Windows.Forms.Button kolaArtiLabel;
+        private System.Windows.Forms.Label kolaSayisiLabel;
+        private System.Windows.Forms.Label kolaLabel;
+        private System.Windows.Forms.Button kahveEksiLabel;
+        private System.Windows.Forms.Button kahveArtiLabel;
+        private System.Windows.Forms.Label kahveSayisiLabel;
+        private System.Windows.Forms.Label kahveLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CafeSiparis/SiparişForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CafeSiparis
+{
+    public partial class SiparişForm : Form
+    {
+        public SiparişForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int caySayisi = Convert.ToInt32(caySayisiLabel.Text);
+            if (caySayisi > 0)
+            {
+                caySayisi--;
+                caySayisiLabel.Text = Convert.ToString(caySayisi);
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("SİPARİŞ ÖZETİ"+"\n"+"\n"+"ÇAY:"+caySayisiLabel.Text + "\n" + "KAHVE:"+kahveSayisiLabel.Text+"\n"+"KOLA:"+kolaSayisiLabel.Text);
+        }
+
+        private void cayArtiLabel_Click(object sender, EventArgs e)
+        {
+            int caysayisi = Convert.ToInt32(caySayisiLabel.Text);
+            caysayisi++;
+            caySayisiLabel.Text = Convert.ToString(caysayisi);
+        }
+
+        private void kahveArtiLabel_Click(object sender, EventArgs e)
+        {
+            int kahvesayisi = Convert.ToInt32(kahveSayisiLabel.Text);
+            kahvesayisi++;
+            kahveSayisiLabel.Text = Convert.ToString(kahvesayisi);
+        }
+
+        private void kahveEksiLabel_Click(object sender, EventArgs e)
+        {
+            int kahvesayisi = Convert.ToInt32(kahveSayisiLabel.Text);
+            if (kahvesayisi > 0)
+            {
+            kahvesayisi--;
+            kahveSayisiLabel.Text = Convert.ToString(kahvesayisi);
+            }
+           
+        }
+
+        private void kolaArtiLabel_Click(object sender, EventArgs e)
+        {
+            int kolasayisi = Convert.ToInt32(kolaSayisiLabel.Text);
+            kolasayisi++;
+            kolaSayisiLabel.Text = Convert.ToString(kolasayisi);
+
+
+        }
+
+        private void kolaEksiLabel_Click(object sender, EventArgs e)
+        {
+            int kolasayisi = Convert.ToInt32(kolaSayisiLabel.Text);
+            if (kolasayisi > 0)
+            { 
+            kolasayisi--;
+            kolaSayisiLabel.Text = Convert.ToString(kolasayisi);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           caySayisiLabel.Text = Convert.ToString(0);     //caySayisiLabel.Text = "0";
+           kahveSayisiLabel.Text = Convert.ToString(0);   //kahveSayisiLabel.Text = "0";
+           kolaSayisiLabel.Text = Convert.ToString(0);    // kolaSayisiLabel.Text = "0";
+
+        }
+    }
+}
